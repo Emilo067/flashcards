2e2d0fedd3ac0283edaa9f9f3623b67c49c3fdbd
Emilo067 <gasanovemil467@gmail.com>
2024-06-06 17:00:22 +0300

    add polymorphic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+import { Button } from '@/components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          element={
+            <Button as={Link} to={'/2'}>
+              BUTTON
+            </Button>
+          }
+          path={'/'}
+        />
+        <Route
+          element={
+            <Button as={Link} to={'/'} variant={'secondary'}>
+              BUTTON
+            </Button>
+          }
+          path={'/2'}
+        />
+      </Routes>
+    </BrowserRouter>
+  )
 }
