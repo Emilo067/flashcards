cd81070968a6ca1793d3637cb4d9530acb5c2b63
Emilo067 <gasanovemil467@gmail.com>
2024-06-27 07:33:59 +0300

    modify button component

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,19 +1,37 @@
 import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 
+import { ArrowBack } from '@/assets/icons'
+import clsx from 'clsx'
+
 import s from './button.module.scss'
 
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
   children: ReactNode
   className?: string
+  disabled?: boolean
   fullWidth?: boolean
-  variant?: 'primary' | 'secondary'
+  icon?: boolean
+  variant?: 'link' | 'primary' | 'secondary' | 'tertiary'
 } & ComponentPropsWithoutRef<T>
 
 export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
-  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+  const {
+    as: Component = 'button',
+    children,
+    className,
+    disabled = false,
+    fullWidth,
+    icon = false,
+    variant = 'primary',
+    ...rest
+  } = props
+
+  const clsxStyle = clsx(s[variant], icon && s.icon, fullWidth && s.fullWidth, className)
 
   return (
-    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
+    <Component className={clsxStyle} disabled={disabled} {...rest}>
+      {icon && <ArrowBack col={'#fff'} />} {children}
+    </Component>
   )
 }
